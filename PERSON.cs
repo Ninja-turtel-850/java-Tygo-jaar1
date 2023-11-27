29080d525e8b58f1881fd66e2e14dbb318acd01a
Ninja-turtel-850 <144018086+Ninja-turtel-850@users.noreply.github.com>
2023-11-27 10:43:30 +0100

    Add files via upload

---
+++ PERSON.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__eerste_project
+{
+    internal class PERSON
+    {
+        public string naam;
+        public int age;
+        public float lengte;
+        public float gewicht;
+        public string haarkleur;
+        public bool bril;
+
+
+    }
+}
